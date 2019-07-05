83b74c6ccc722c36483d3d11c10f38ded5038a3d
Timofte Ciprian-Andrei <timofteca@gmail.com>
2019-07-05 14:11:40 +0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,4 @@
 define(function(require) {
-  // Load any app-specific modules
-  // with a relative require call,
-  // like:
   var navbar = require("./navbar");
   var data = require("./displayData");
 
